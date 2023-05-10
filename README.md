3274316e4beb16093c96bb00ee064c2ad5063015
Marni Luka <marnixluka@gmail.com>
2023-05-09 23:15:34 -0400

    finished README

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ Before starting, please make sure to add your MySQL password into the server.js 
 To view your database, you need to run 'node server.js' into your terminal. From there, you will see a list of options. Simply choose the one you wish to use, and follow any given prompts.
 
 A [demonstration video](https://drive.google.com/file/d/1oksarSHrwx5SdCvtvG7r9nQY2ywMFRAu/view) is provided.
+
+## Extra
+The Update an Employee Role option needs to be fixed as it currently doesn't work.
+Also need to figure out how to make it so the choices can be based on a name instead of it's id number, so it's you're not left guessing what the id numbers are referring to.
--- db/seeds.sql
+++ db/seeds.sql
@@ -6,21 +6,21 @@ VALUES ("Hobbit"),
         ("Wizard");
 
 INSERT INTO role (title, salary)
-VALUES ("Leader", 250000),
-        ("Ring Bearer", 300000),
-        ("Swordsman", 200000),
-        ("Archer", 150000),
-        ("Axeman", 150000),
-        ("The True Hero", 500000),
-        ("Comedic Relief", 100000);
+VALUES ("Leader", 250000, 5),
+        ("Ring Bearer", 300000, 1),
+        ("Swordsman", 200000, 2),
+        ("Archer", 150000, 3),
+        ("Axeman", 150000, 4),
+        ("The True Hero", 500000, 1),
+        ("Comedic Relief", 100000, 1);
     
-INSERT INTO employee (first_name, last_name, manager_id)
-VALUES ("Frodo", "Baggins", 2),
-        ("Samwise", "Gamgee", 8),
-        ("Pippin", "Took", 8),
-        ("Merry", "Brandybuck", 8),
-        ("Aragorn", "son of Arathorn", 8),
-        ("Legolas", "Greenleaf", 5),
-        ("Gimli", "son of Gloin", 5),
-        ("Gandalf", "the White", NULL),
-        ("Boromir", "son of Denethor", 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Frodo", "Baggins", 2, 2),
+        ("Samwise", "Gamgee", 6, 8),
+        ("Pippin", "Took", 7, 8),
+        ("Merry", "Brandybuck", 7, 8),
+        ("Aragorn", "son of Arathorn", 3, 8),
+        ("Legolas", "Greenleaf", 4, 5),
+        ("Gimli", "son of Gloin", 5, 5),
+        ("Gandalf", "the White", 1, NULL),
+        ("Boromir", "son of Denethor", 3, 5);
